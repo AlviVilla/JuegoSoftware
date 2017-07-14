0d52752ec2cb5e851bd665f0061151791f829533
AlviVilla <a.villa6@hotmail.com>
2017-07-14 14:43:45 +0200

    tipos en NPC

--- JuegoSoftware/src/fighter/NPC.java
+++ JuegoSoftware/src/fighter/NPC.java
@@ -10,16 +10,18 @@ import strategy.Strategy;
 
 public class NPC extends Fighter{
 	Random randomStrategy	 = new Random(System.currentTimeMillis());
-	public NPC(int lvl){
+	public NPC(int lvl, String tipo){
 		super.stats = new Stats(lvl);
 		int rand = randomStrategy.nextInt(3);
-		
+		super.tipo=tipo;
 		if(rand==0)
 			super.strategy = new Normal();
 		if(rand==1)
 			super.strategy = new Agressive();
 		if(rand==2)
 			super.strategy = new Defensive();
-		
+		}
+	public String getDecorador(){
+		return super.getDecorador();
 	}
 }
